d3fa0a8f265b425272dd2a3ce83e2132a682d2f6
Islam Shamshiev <55310909+Salaga911@users.noreply.github.com>
2023-10-02 23:08:03 +0600

    Initial commit

--- main.py
+++ main.py
@@ -6,41 +6,66 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
-
-bot = Bot(os.getenv('API_KEY_TELE'))
-dp = Dispatcher(bot=bot)
-
-main = ReplyKeyboardMarkup(resize_keyboard=True)
-main.add('Информация').add('Инструкция')
-
 main_trello_end_point = os.getenv('URL')
 trello_key = os.getenv('API_KEY')
 trello_token = os.getenv('TOKEN')
 application_list_id = os.getenv('ID')
 
-first_run = True
+bot = Bot(os.getenv('API_KEY_TELE'))
+dp = Dispatcher(bot=bot)
+
+main = ReplyKeyboardMarkup(resize_keyboard=True)
+main.add('ℹ️Инструкция')
 
 
 @dp.message_handler(commands=['start'])
 async def process_start_command(msg: types.Message):
-    global first_run
-    if first_run:
-        await msg.answer(f"Добро пожаловать {msg.from_user.first_name}", reply_markup=main)
-        await msg.answer('При помощи этого бота вы можете обращаться в технический отдел!')
-        first_run = False
+    await bot.forward_message(-1001792269419, msg.from_user.id, msg.message_id, msg.from_user.first_name)
+    text_salam = """
+                👋Добро пожаловать!🎉
+                
+                🤖Этот бот поможет вам обратиться в технический отдел.👨‍💻
+                
+                ❓Чтобы обратиться в технический отдел, просто задайте свой вопрос или проблему.💡
+                
+                📨Бот передаст ваше сообщение сотруднику технического отдел.⏳
+                
+                
+                ℹ️ Инструкция ⬇️
+                
+                Пример запроса:
+                
+                Технический отдел, Иванов Иван, Проблема с компьютером
+                
+                Внимание:
+                
+                Отправляйте запросы в формате: [отдел] [фамилия] [имя] [проблема] 📝
+                
+                Не отправляйте стикеры, голосовые сообщения и другие медиафайлы 🔇
+                """
+    bot_picture = open("botpic.jpg", 'rb')
+    await msg.answer_photo(photo=bot_picture, caption=text_salam, reply_markup=main)
 
 
-@dp.message_handler(text='Информация')
+@dp.message_handler(text='ℹ️Инструкция')
 async def cmd_instructions(msg: types.Message):
-    logo = open('logo.png', 'rb')
-    await msg.answer_photo(photo=logo, caption='hello')
-
-
-@dp.message_handler(text='Инструкция')
-async def cmd_info(msg: types.Message):
-    await msg.answer("Пример как писать запрос ⤵\n"
-                     "Отдел продаж, сотрудник Лев Яшин, нет подключение к сети\n"
-                     "--------------------------------------------------------------------------------")
+    logo = open('Rule.jpg', 'rb')
+    info_text = """
+                Пример запроса:
+                
+                Технический отдел, Иванов Иван, Проблема с компьютером
+                
+                Внимание:
+                
+                Отправляйте запросы в формате: [отдел] [фамилия] [имя] [проблема] 📝
+                
+                ❌
+                
+                Не отправляйте стикеры, голосовые сообщения и другие медиафайлы 🔇
+                
+                ❌
+                """
+    await msg.answer_photo(photo=logo, caption=info_text)
 
 
 def create_trello_card(card_name, card_desc):
@@ -57,12 +82,14 @@ def create_trello_card(card_name, card_desc):
 
 @dp.message_handler()
 async def echo_message(msg: types.Message):
+    await bot.forward_message(-1001792269419, msg.from_user.id, msg.message_id, msg.from_user.first_name)
     print(msg.text.split(' '))
-    await msg.reply("Запрос отправлен\n"
-                    "в течении 30 минут заявка будет принято")
+    await bot.send_video(msg.chat.id, open('Gifask.gif', 'rb'),
+                         caption="🤖Ваша заявка будет обработана в течение 15 минут!!!🤖")
     list_accept = []
     for i in msg.text.split():
         list_accept.append(i)
+
     print(list_accept[0:2], list_accept[2::])
     print(create_trello_card(' '.join(list_accept[0:2]), ' '.join(list_accept[2::])))
 
